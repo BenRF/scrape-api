a0b5b1b659743ebf2e8c05b6c9ea3348d1118676
Ben Ramsay Foster <benramsayfoster@gmail.com>
2023-10-15 14:15:45 +0100

    Added request parameter to choose which browser is used

--- functions/api.js
+++ functions/api.js
@@ -3,12 +3,14 @@ const bodyParser = require('body-parser');
 
 const StatusEndpoint = require('./endpoints/status');
 const ScrapeEndpoint = require('./endpoints/scrape');
+const BrowserManager = require('./browserManager');
 const Router = require('./router');
 
 const router = new Router();
+const browserManager = new BrowserManager();
 
 router.createGet('/status', new StatusEndpoint());
-const Scraper = new ScrapeEndpoint();
+const Scraper = new ScrapeEndpoint(browserManager);
 router.createPost('/scrape', Scraper);
 
 const app = express();
@@ -22,5 +24,5 @@ app.listen(3000, () => {
 });
 
 process.on('exit', () => {
-  Scraper.stop();
+  browserManager.stop();
 });
